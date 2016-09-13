a44b1fe7df9888d6bce5a226198da2f7dda69338
musaomondi <amokem@yahoo.com>
2016-09-13 12:41:44 +0300

    generating controller named home

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessible :email, :name, :password, :password_confirmation, :remember_me
   validates :email, presence: :true, uniqueness: :true
 end
