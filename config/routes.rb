d6aedf58812e8d61db2cb9101696be4f40372055
musaomondi <amokem@yahoo.com>
2016-09-16 07:45:13 +0300

    solving routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
   devise_for :users
+
+  devise_scope :user do
+       get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root :to => "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
